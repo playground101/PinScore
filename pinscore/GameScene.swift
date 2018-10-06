d42b651230eb5dadc232a24234d8edfa72a85462
Inf3rn065 <kbhagat0618@gmail.com>
2018-10-06 14:28:29 -0700

    restricted ball to top of screen

--- pinscore/GameScene.swift
+++ pinscore/GameScene.swift
@@ -15,31 +15,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate{
 
     override func didMove(to view: SKView) {
     ball = self.childNode(withName: "ball") as? SKSpriteNode
-        ball?.position = CGPoint(x: 0, y: self.frame.height/2 + 50)
-        self.physicsWorld.contactDelegate = self
+                self.physicsWorld.contactDelegate = self
 
 }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         let location = touches.first?.location(in: self)
-        ball?.position = location!
-        createApple()
+       // ball?.position = location!
+        createApple(point: CGPoint(x: CGFloat((location?.x)!), y: CGFloat(self.frame.height/2)) )
     }
     
-    func createApple() {
+    func createApple(point: CGPoint) {
         ball = SKSpriteNode(imageNamed:"Circle")
         ball?.size = CGSize(width: 50, height: 50)
-        ball?.position = CGPoint(x: 0, y: -460)
+        ball?.position = point
         ball?.physicsBody = SKPhysicsBody(circleOfRadius: (ball?.size.width)!/2)
         self.addChild(ball!)
-    }
-    
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            ball?.position = location
-        }
     }
 
 }
