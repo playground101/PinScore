4861ae7154526d51f8bce9c1ec3a88a65b5314ea
playground101 <playgrounddfw@gmail.com>
2018-12-16 12:16:01 -0600

    Fixed close button for credits

--- pinscore/CreditsScene/CreditsScene.swift
+++ pinscore/CreditsScene/CreditsScene.swift
@@ -13,5 +13,24 @@ import GameplayKit
 
 class CreditsScene: SKScene {
     var credits: SKLabelNode?
+    var close: SKLabelNode?
+    
+    
+    override func didMove(to view: SKView) {
+        close = self.childNode(withName: "close") as? SKLabelNode
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let points = touch.location(in: self)
+            if close!.contains(points) {
+                // animation(node: close!)
+                close!.removeFromParent()
+                let startScene = StartScene(fileNamed: "StartScene")
+                startScene?.scaleMode = .aspectFit
+                self.view?.presentScene(startScene!)
+            }
+        }
+    }
     
 }
